44b26d91150ef3c99d8a7dc2d2b58313a1ff8230
ethanmoises <110655737+ethanmoises@users.noreply.github.com>
2025-09-19 02:47:39 +0800

    Update Main.java

--- LibraryBookSystem/Main.java
+++ LibraryBookSystem/Main.java
@@ -7,7 +7,7 @@ public class Main {
         int choice;
 
         do {
-            // Display menu options
+           
             System.out.println("\nLibrary Book System Menu:");
             System.out.println("1. Add Record");
             System.out.println("2. Delete Record");
