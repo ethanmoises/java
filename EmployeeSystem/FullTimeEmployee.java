09c2ca7210caa52422fcebe5217c3437be0b6f85
ethanmoises <110655737+ethanmoises@users.noreply.github.com>
2025-09-19 02:31:16 +0800

    Add files via upload

---
+++ EmployeeSystem/FullTimeEmployee.java
@@ -0,0 +1,6 @@
+public class FullTimeEmployee extends Employee{
+    FullTimeEmployee(String name, int employeeId, String position, String department) {
+            super(name, employeeId, position, department);
+
+        }
+}
