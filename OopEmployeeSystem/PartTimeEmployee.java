4b4578b08a56c4b6bdda5860054300ea7a0298c4
ethanmoises <110655737+ethanmoises@users.noreply.github.com>
2025-09-19 02:27:52 +0800

    Add files via upload

---
+++ OopEmployeeSystem/PartTimeEmployee.java
@@ -0,0 +1,7 @@
+public class PartTimeEmployee extends Employee{
+
+    PartTimeEmployee(String name, int employeeId, String position, String department) {
+        super(name, employeeId, position, department);
+
+    }
+}
