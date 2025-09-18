6f1bbde895563143ae2ad4a21243dff22e19b1e5
ethanmoises <110655737+ethanmoises@users.noreply.github.com>
2025-09-19 02:30:08 +0800

    Delete OopEmployeeSystem directory

--- OopEmployeeSystem/FullTimeEmployee.java
+++
@@ -1,6 +0,0 @@
-public class FullTimeEmployee extends Employee{
-    FullTimeEmployee(String name, int employeeId, String position, String department) {
-            super(name, employeeId, position, department);
-
-        }
-}
--- OopEmployeeSystem/PartTimeEmployee.java
+++
@@ -1,7 +0,0 @@
-public class PartTimeEmployee extends Employee{
-
-    PartTimeEmployee(String name, int employeeId, String position, String department) {
-        super(name, employeeId, position, department);
-
-    }
-}
